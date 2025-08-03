1c145612922c9da27c3f7fba4574731301a3f01f
Lilia <lg.amirova79@gmail.com>
2025-08-03 14:38:11 +0300

    Урок-3: автотест с использованием паттерна PageObject. Добавила автотест ввод логина без пароля

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -1,10 +1,11 @@
 # из которого будут наследовать все тесты-это родительский
-
 import pytest
 from selenium import webdriver
+import time
 
 @pytest.fixture(scope='session')
 def browser(): # отвечает за работу браузеров: открытие и закрытие
     driver = webdriver.Chrome() # запускается окно браузера
     yield driver                # ставим дальнейшее выполнение функции на паузу
+    time.sleep(3)               # чтоб успеть постмотреть что происходит на странице после выполнения функции
     driver.quit()               # закрытие браузера
