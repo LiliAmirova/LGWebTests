01180c6ab2512d3828a97ac774a102ba4f66e541
Lilia <lg.amirova79@gmail.com>
2025-08-03 20:02:17 +0300

    Урок-2-1: Добавили файл pytest.ini

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -9,13 +9,15 @@ EMPTY_LOGIN_ERROR = 'Введите логин'
 EMPTY_PASSWORD_ERROR = 'Введите пароль'
 
 @allure.suite('Проверка формы авторизации')
-@allure.title('Проверка ошибки при рустой форме авторизации')
+@allure.title('Проверка ошибки при пустой форме авторизации')
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.suite('Проверка формы авторизации')
+@allure.title('Проверка ошибки при пустом поле пароль')
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
